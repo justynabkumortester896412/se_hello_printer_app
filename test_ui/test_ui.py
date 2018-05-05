a8df4f9f2ee8fa5db3bea4d607205dd43dc16dbd
justynabkumortester896412 <justynabkumortester896412@users.noreply.github.com>
2018-05-05 14:19:22 +0200

    test_ui

---
+++ test_ui/test_ui.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import unittest
+import time
+import pytest
+
+
+@pytest.mark.uitest
+class TestFormater(unittest.TestCase):
+    def test_plain_lowercase(self):
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:5000?output=html")
+        time.sleep(100)
+        driver.quit()
